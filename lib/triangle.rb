040e42a0ba5a6c530554238fc1db4b5c9dc538a9
Jade Lynn Masker <hopefulfire@gmail.com>
2020-01-24 15:36:35 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  
+	def initialize(side1, side2, side3)
+		@side1 = side1
+		@side2 = side2
+		@side3 = side3
+	end
+
+	def kind
+		if @side1 == 0 || @side2 == 0 || @side3 == 0
+			raise TriangleError #sides cannot be zero
+		elsif @side1 >= @side2 + @side3 || @side2 >= @side3 + @side1 || @side3 >= @side1 + @side2
+			raise TriangleError #sides cannot be equal or greater than the sum of the other two sides
+		end
+		if @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+			if @side1 == @side2 && @side2 == @side3
+				:equilateral
+			else
+				:isosceles
+			end
+		else
+			:scalene
+		end
+	end
+
+	class TriangleError < StandardError
+
+		def message
+			"Impossible triangle."
+		end
+
+	end
+
 end
