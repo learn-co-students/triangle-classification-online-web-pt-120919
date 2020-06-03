79d7546b527273f152ba3074163ec6960b338f7b
fasnimo <fasnimo@gmail.com>
2020-06-02 21:06:05 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
+    if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2)
+      raise TriangleError
+    end 
+
+    if side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError
+    end 
+
+    if side1 == nil || side2 == nil || side3 == nil
+      raise TriangleError
+    end 
+  end 
+
+  def kind 
+    if @side1 == @side2 && @side1 == @side3 && @side2 == @side3
+      return :equilateral
+
+    elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+      return :isosceles
+    else  
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "Name Error, please enter the right type of triangle!"
+  end
 end
